9a9c9b853a20b4659fdb7a385ab7c00d9ff51e37
Rita Madio <160418348+ritamad@users.noreply.github.com>
2024-06-18 18:59:38 +0200

    feat: add getComputerChoice() function

---
+++ script.js
@@ -0,0 +1,15 @@
+//determine computer choice
+function getComputerChoice() {
+    let random = Math.random();
+    if(random >= 0 && random <= 0.32) {
+        random = "Rock";
+    } else if (random >= 0.33 && random <= 0.66) {     //Paper has a higher chance (just one more)
+        random = "Paper";
+    } else {
+        random = "Scissors";
+    }
+    return random;
+}
+
+
+
