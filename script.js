933b25bfba2fb8396225bf5863f285c7744c58db
Rita Madio <160418348+ritamad@users.noreply.github.com>
2024-06-19 11:34:18 +0200

    feat: add playRound() function to play a single round

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 function getComputerChoice() {
     let random = Math.random();
     if(random >= 0 && random <= 0.32) {
-        random = "Rock";
-    } else if (random >= 0.33 && random <= 0.66) {     //Paper has a higher chance (just one more)
-        random = "Paper";
+        random = "rock";
+    } else if (random >= 0.33 && random <= 0.66) {     //Paper has a higher chance (just 1% more)
+        random = "paper";
     } else {
-        random = "Scissors";
+        random = "scissors";
     }
     return random;
 }
+
 
 //get human choice 
 function getHumanChoice() {
@@ -22,3 +23,21 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+
+// function to play a single round
+function playRound(humanChoice2, computerChoice2) {
+    if ((humanChoice2 === "rock" && computerChoice2 === "scissors") || (humanChoice2 === "paper" && computerChoice2 === "rock") || (humanChoice2 === "scissors" && computerChoice2 === "paper")) {
+        console.log(`you win! ${humanChoice2} beats ${computerChoice2}`);
+        humanScore += 1;
+    } else if ((humanChoice2 === "rock" && computerChoice2 === "paper") || (humanChoice2 === "paper" && computerChoice2 === "scissors") || (humanChoice2 === "scissors" && computerChoice2 === "rock")) {
+        console.log(`you lose! ${computerChoice2} beats ${humanChoice2}`);
+        computerScore += 1;
+    } else {
+        console.log("tie!");
+    }
+  }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
